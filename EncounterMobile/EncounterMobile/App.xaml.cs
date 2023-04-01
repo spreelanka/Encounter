8178e4efaa9cceee20ca4a049923b48b9509cc7b
Jacob Hudson <jacob@portablefoundations.com>
2023-04-01 14:12:43 -0400

    initial project

---
+++ EncounterMobile/EncounterMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EncounterMobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ EncounterMobile/EncounterMobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EncounterMobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
