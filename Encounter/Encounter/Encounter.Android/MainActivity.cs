da025e8519a02656d0951e5d73b58935244d9c94
Jacob Hudson <jacob@portablefoundations.com>
2023-04-02 17:18:31 -0400

    api service work

--- Encounter/Encounter/Encounter.Android/MainActivity.cs
+++ Encounter/Encounter/Encounter.Android/MainActivity.cs
@@ -14,6 +14,7 @@ namespace Encounter.Droid
         {
             base.OnCreate(savedInstanceState);
 
+            Rg.Plugins.Popup.Popup.Init(this);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
--- Encounter/Encounter/Encounter/App.xaml.cs
+++ Encounter/Encounter/Encounter/App.xaml.cs
@@ -3,30 +3,54 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using Encounter.Services;
 using Encounter.Views;
+using Prism;
+using Prism.Ioc;
+using Prism.Unity;
+using Prism.Plugin.Popups;
+using Encounter.Services.Interfaces;
+using System.Threading.Tasks;
 
 namespace Encounter
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        public App() : this(null) { }
 
-        public App ()
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
         {
             InitializeComponent();
-
-            DependencyService.Register<MockDataStore>();
-            MainPage = new AppShell();
+            await NavigationService.NavigateAsync(nameof(AboutPage));
         }
 
-        protected override void OnStart ()
+        protected override void OnStart()
         {
+            base.OnStart();
+
+            Xamarin.Essentials.Connectivity.ConnectivityChanged += Connectivity_ConnectivityChanged;
         }
 
-        protected override void OnSleep ()
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            // This updates INavigationService and registers PopupNavigation.Instance
+            containerRegistry.RegisterPopupNavigationService();
+            containerRegistry.RegisterSingleton<IMonsterService, MonsterService>();
+            containerRegistry.RegisterSingleton<IEncounterService, EncounterService>();
+            containerRegistry.RegisterForNavigation<AboutPage>();
+            containerRegistry.RegisterForNavigation<LoginPage>();
         }
 
-        protected override void OnResume ()
+        void Connectivity_ConnectivityChanged(object sender, Xamarin.Essentials.ConnectivityChangedEventArgs e)
         {
+            if (e.NetworkAccess == Xamarin.Essentials.NetworkAccess.Internet)
+            {
+                //have network
+            }
+            else
+            {
+                //no network
+            }
         }
     }
 }
