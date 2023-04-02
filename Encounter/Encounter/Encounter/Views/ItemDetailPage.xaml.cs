caf79ea34066a5f6776a73eb74ecfba9ed66f131
Jacob Hudson <jacob@portablefoundations.com>
2023-04-02 12:37:41 -0400

    x.f

---
+++ Encounter/Encounter/Encounter/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Encounter.ViewModels;
+
+namespace Encounter.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
